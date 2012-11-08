39b6a5fa50b5115d702e473564373339413b0dee
Jean-François Roche <jfroche@affinitic.be>
2012-11-08 12:23:42 +0100

    add session helper function

--- gites/db/__init__.py
+++ gites/db/__init__.py
@@ -1,13 +1,24 @@
 from zope.component import getUtility
+from affinitic.db.interfaces import IDatabase
 from affinitic.pwmanager.interfaces import IPasswordManager
-from z3c.sqlalchemy import createSAWrapper
+from z3c.sqlalchemy import createSAWrapper, getSAWrapper
 
+def session():
+    try:
+        wrapper = getSAWrapper('gites_wallons')
+    except ValueError:
+        db = getUtility(IDatabase, 'postgres')
+        return db.session
+    else:
+        return wrapper.session
+
+from gites.db.content import *
 
 def initialize(context):
     pwManager = getUtility(IPasswordManager, 'pg')
     connString = 'postgres://%s@localhost/gites_wallons' % \
                         pwManager.getLoginPassWithSeparator(':')
-    wr = createSAWrapper(connString,
+    createSAWrapper(connString,
                         forZope=True,
                         engine_options = {'convert_unicode': True,
                                           'encoding': 'utf-8'},
