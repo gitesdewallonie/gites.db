f9e63970cc2c5c82242d24d3fb26466903bc2aa9
Jean-François Roche <jfroche@affinitic.be>
2013-03-05 11:14:21 +0100

    fix acquisition on hebergement

--- gites/db/mapper.py
+++ gites/db/mapper.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+from Acquisition import ImplicitAcquisitionWrapper
 from z3c.sqlalchemy.mapper import MappedClassBase
 from gites.db import DeclarativeBase
 
 
 class GitesMappedClassBase(DeclarativeBase, MappedClassBase):
     __abstract__ = True
+
+    def __of__(self, wrapper):
+        return ImplicitAcquisitionWrapper(self, wrapper)
