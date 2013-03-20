bb2ddbfe58dc37e759737838839410cc2a0894aa
schminitz <gillian@affinitic.be>
2013-03-20 18:07:45 +0100

    override url to access password file in scripts

--- gites/db/pg.py
+++ gites/db/pg.py
@@ -6,8 +6,15 @@ Licensed under the GPL license, see LICENCE.txt for more details.
 Copyright by Affinitic sprl
 """
 from affinitic.db.pg import PGDB
+from zope.component import getUtility
+from affinitic.pwmanager.interfaces import IPasswordManager
 
 
 class GitesDB(PGDB):
     db = 'gites_wallons'
     verbose = True
+
+    @property
+    def url(self):
+        pwManager = getUtility(IPasswordManager, 'pg')
+        return 'postgres://%s@localhost/gites_wallons' % pwManager.getLoginPassWithSeparator(':')
