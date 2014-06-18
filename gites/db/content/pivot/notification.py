ea76dd183ee5acd5ebe8ff875c52387bc65565d8
schminitz <gillian@affinitic.be>
2014-06-18 15:38:43 +0200

    Add some methods to mapper ##

--- gites/db/content/pivot/notification.py
+++ gites/db/content/pivot/notification.py
@@ -28,9 +28,9 @@ class PivotNotification(GitesMappedClassBase):
                                sa.String(),
                                nullable=False)
 
-    not_date = sa.Column('notf_date',
-                         sa.DateTime(),
-                         nullable=False)
+    notf_date = sa.Column('notf_date',
+                          sa.DateTime(),
+                          nullable=False)
 
     notf_treated = sa.Column('notf_treated',
                              sa.Boolean())
@@ -48,3 +48,21 @@ class PivotNotification(GitesMappedClassBase):
                              sa.Integer(),
                              primary_key=True,
                              unique=True)
+
+    def getUntreatedNotifications(self, origin):
+        """
+        Return untreated or unapplied notifications depeding on the origin
+        """
+        pass
+
+    def getNotifications(self, origin):
+        """
+        Return all notifications depending on the origin
+        """
+        pass
+
+    def treatNotification(self, pk, cmt, user):
+        """
+        Treat notification
+        """
+        pass
