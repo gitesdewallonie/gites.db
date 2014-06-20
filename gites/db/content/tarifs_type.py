84b687901da9fd7054d5fc42ab84ab013f065acb
Martin Peeters <martin.peeters@affinitic.be>
2014-06-20 15:01:57 +0200

    Add mappers for Tarifs and TarifsType table

---
+++ gites/db/content/tarifs_type.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+"""
+gites.db
+
+Created by mpeeters
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+"""
+
+from gites.db.mappers import GitesMappedClassBase
+import sqlalchemy as sa
+
+
+class TarifsType(GitesMappedClassBase):
+    __tablename__ = 'tarifs_type'
+
+    type = sa.Column('type', sa.String, primary_key=True, nullable=False)
+
+    subtype = sa.Column('subtype', sa.String, primary_key=True, nullable=False)
