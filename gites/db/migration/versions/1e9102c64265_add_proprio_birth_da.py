b44076bcb0d93d317346ca568b2d3ba0356dbd88
schminitz <gillian@affinitic.be>
2015-10-28 12:31:52 +0100

    Add prod_date_naiss to proprio and proprio_maj migration refs #6289

---
+++ gites/db/migration/versions/1e9102c64265_add_proprio_birth_da.py
@@ -0,0 +1,27 @@
+"""Add proprio birth date
+
+Revision ID: 1e9102c64265
+Revises: 1de3a443694f
+Create Date: 2015-10-28 11:58:56.563965
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1e9102c64265'
+down_revision = '1de3a443694f'
+
+import sqlalchemy as sa
+
+
+def upgrade():
+    print "... Add prod_date_naiss to proprio and proprio_maj"
+    op.add_column('proprio',
+                  sa.Column('pro_date_naiss', sa.Date))
+    op.add_column('proprio_maj',
+                  sa.Column('pro_maj_date_naiss', sa.Date))
+
+
+def downgrade():
+    print "... Drop pro_date_naiss to proprio and proprio_maj"
+    op.drop_column('proprio', 'pro_date_naiss')
+    op.drop_column('proprio_maj', 'pro_maj_date_naiss')
